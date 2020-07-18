587b9af185aa4f38dcb0e756e1614efb14b69b42
Mauricio Dottavio <dottavio@gmail.com>
2020-07-17 23:40:24 -0300

    fix: linking to source code.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fetchData = async () => {
 const generateDoc = ({ data }) => {
   const table = new Table({
     head: ['Currently infected', 'Recovered Cases', 'Death Cases']
-    , colWidths: [25, 25, 25]
+    , colWidths: [35, 35, 35]
   });
   table.push([data.currently_infected, data.recovery_cases, data.death_cases]);
   return `
@@ -26,11 +26,13 @@ Recovered Percentage ${data.closed_cases_recovered_percentage}% 😌
 
 ### Please, use your Mask 😷
 
-> Last update: ${data.last_update}
-
 #### Hi there 👋
 I'm Mauricio, I wanted to showcase the power of Github's workflow while sending a message to those who landed here.
+If you're interested in seeing how this work, check the source code of [the workflow](https://github.com/mdottavio/mdottavio/blob/master/.github/workflows/updateReadme.yml) that runs periodically, firing
+the [Node script](https://github.com/mdottavio/mdottavio/tree/covidstats) that fetch and format the data.
 
+> Last update: ${data.last_update}
+>
 > Data from [corona-virus-stats.herokuapp.com](https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats).
 `;
 }
